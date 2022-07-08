63aa33866d930524735b5f0e5292646c51115e5b
Edmund <edmundlzen@gmail.com>
2022-07-08 10:13:58 +0800

    Added constants folder and constant file, 'subject'

---
+++ src/constants/subjects.ts
@@ -0,0 +1,16 @@
+const subjects: { [key: string]: string } = {
+	mathematics: 'Mathematics',
+	english: 'English',
+	malay: 'Malay',
+	chinese: 'Chinese',
+	history: 'History',
+	tamil: 'Tamil',
+	physics: 'Physics',
+	biology: 'Biology',
+	chemistry: 'Chemistry',
+	science: 'Science',
+	modern_mathematics: 'Modern Mathematics',
+	additional_mathematics: 'Additional Mathematics',
+}
+
+export default subjects;
