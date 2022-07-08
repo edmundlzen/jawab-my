6f39e50187d4212061c6765bbc4fc61c6296a86b
Edmund <edmundlzen@gmail.com>
2022-07-08 10:19:26 +0800

    Using Layout component in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,15 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { trpc } from "../utils/trpc";
+import { Layout } from "../components/Layout";
 
 const Home: NextPage = () => {
   const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
 
   return (
-    <>
+    <Layout>
       <div className="text-center text-9xl text-blue-900">Hello</div>
-    </>
+    </Layout>
   );
 };
 
