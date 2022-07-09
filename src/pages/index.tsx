8e224cc397bea8281f70a4b769693076db0a4ae0
Edmund <edmundlzen@gmail.com>
2022-07-10 01:55:57 +0800

    Using QuestionViewComponent in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,20 +2,17 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { trpc } from "../utils/trpc";
 import { Layout } from "../components/Layout";
+import { QuestionView } from "../components/QuestionsView";
+import { InferQueryOutput } from "../utils/trpc";
 
 const Home: NextPage = () => {
   const questions = trpc.useQuery(["questions.getAll"]);
 
   return (
     <Layout>
-      <div className="text-center text-9xl text-blue-900">Hello</div>
-      {questions.data &&
-        questions.data.map((question) => (
-          <div key={question.id}>
-            <div className="text-2xl text-blue-900">{question.title}</div>
-            <div className="text-xl text-blue-900">{question.content}</div>
-          </div>
-        ))}
+      {questions.data && (
+        <QuestionView questions={questions.data} title="Questions" />
+      )}
     </Layout>
   );
 };
