b811631f0d5869169a78460d272283f6c1530163
Edmund <edmundlzen@gmail.com>
2022-07-10 14:16:18 +0800

    Added loading indicator when questions are loading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,17 @@ import Head from "next/head";
 import { trpc } from "../utils/trpc";
 import { Layout } from "../components/Layout";
 import { QuestionView } from "../components/QuestionsView";
-import { InferQueryOutput } from "../utils/trpc";
+import { useEffect } from "react";
+import { useLoading } from "../hooks";
 
 const Home: NextPage = () => {
   const questions = trpc.useQuery(["questions.getAll"]);
+  const { setLoading } = useLoading();
+
+  useEffect(() => {
+    if (!setLoading) return;
+    setLoading(questions.isLoading);
+  }, [questions, setLoading]);
 
   return (
     <Layout>
