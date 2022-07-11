a9750908d12052c06cab727ce8b9556b21757e64
Edmund <edmundlzen@gmail.com>
2022-07-11 10:32:53 +0800

    Added view posts by subject page

--- src/pages/questions/[subject]/index.tsx
+++ src/pages/questions/[subject]/index.tsx
@@ -1,121 +1,57 @@
-import {NextPage} from "next";
-import {Layout} from "../../../components/Layout";
-import {gql} from "graphql-request";
-import {db} from "../../../db";
-import {QuestionView} from "../../../components/QuestionsView";
-import {Text} from "@mantine/core";
-
-interface Question {
-	id: string;
-	title: string;
-	content: string;
-	tags: string[];
-	subject: string;
-	form: string;
-	user_id: string;
-	created_at: string;
-	updated_at: string;
-	votes: number;
-	views_count: number
-	answers_count: number
-	user: {
-		profile_image_url: string
-		username: string
-	}
-}
-
-export const getStaticPaths = async () => {
-	return {
-		paths: [],
-		fallback: true,
-	};
-}
-
-const getQuestions = async (limit: number, subject: string): Promise<Question[]> => {
-    const QUESTIONS_QUERY = gql`
-        {
-            question(order_by: {created_at: asc}, limit: ${limit}, where: {subject: {_eq: ${subject}}}) {
-                id
-                subject
-                title
-                content
-                form
-                tags
-                created_at
-                votes {
-                    type
-                }
-                views_aggregate {
-                    aggregate {
-                        count
-                    }
-                }
-                answers_aggregate {
-                    aggregate {
-                        count
-                    }
-                }
-                user {
-                    profile_image_url
-                    username
-                }
-            }
-        }
-	`;
-	const data = await db.request(QUESTIONS_QUERY);
-
-	return data.question.map((question: any) => {
-		return {
-			...question,
-			votes: question.votes.reduce(((acc: number, vote: { type: string }) => {
-				if (vote.type === 'up') {
-					return acc + 1;
-				}
-				return acc - 1;
-			}), 0),
-			views_count: question.views_aggregate.aggregate.count,
-			answers_count: question.answers_aggregate.aggregate.count,
-			user: question.user
-		}
-	});
-}
-
-export const getStaticProps = async ({params}: { params: { subject: string } }) => {
-	const {subject} = params;
-	const questions = await getQuestions(20, subject);
-
-	return {
-		props: {
-			subject: subject.replaceAll('_', ' '),
-			questions,
-		},
-	}
-}
-
+import { NextPage } from "next";
+import { Layout } from "../../../components/Layout";
+import { trpc } from "../../../utils/trpc";
+import { QuestionView } from "../../../components/QuestionsView";
+import { Text } from "@mantine/core";
+import { useRouter } from "next/router";
+import { Subject } from "@prisma/client";
+import { useEffect } from "react";
+import { useLoading } from "../../../hooks";
 
 interface SubjectViewProps {
-	subject: string;
-	questions: Question[] | null;
+    // subject: string;
+    // questions: Question[] | null;
 }
 
 const SubjectView: NextPage<SubjectViewProps> = (props) => {
-	return (
-		<Layout>
-			{
-				props.questions && props.questions.length > 0 ?
-					<QuestionView title={props.subject} questions={props.questions}/>
-					:
-					<div className={'h-full flex flex-col justify-center items-center'}>
-						<Text className={'text-3xl font-semibold text-blue-600'}>
-							No questions found
-						</Text>
-						<Text className={'mt-2 text-lg text-gray-600'}>
-							There are no questions for <span className={'capitalize'}>{props.subject}</span> yet.
-						</Text>
-					</div>
-			}
-		</Layout>
-	)
-}
+    const router = useRouter();
+    const questions = trpc.useQuery([
+        "questions.getBySubject",
+        { subject: router.query.subject as Subject },
+    ]);
+    const { setLoading } = useLoading();
+
+    useEffect(() => {
+        setLoading(questions.isLoading);
+    }, [questions, setLoading]);
+
+    return (
+        <Layout>
+            {questions.data && questions.data.length > 0 ? (
+                <QuestionView
+                    title={router.query.subject as string}
+                    questions={questions.data}
+                />
+            ) : (
+                <div
+                    className={
+                        "h-full flex flex-col justify-center items-center"
+                    }
+                >
+                    <Text className={"text-3xl font-semibold text-blue-600"}>
+                        No questions found
+                    </Text>
+                    <Text className={"mt-2 text-lg text-gray-600"}>
+                        There are no questions for{" "}
+                        <span className={"capitalize"}>
+                            {router.query.subject as string}
+                        </span>{" "}
+                        yet.
+                    </Text>
+                </div>
+            )}
+        </Layout>
+    );
+};
 
 export default SubjectView;
